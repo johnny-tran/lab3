fc22db226810cc931f6bc5bcc2a7c7d64c080e91
Jonathan Tran <jpt017@ucsd.edu>
2019-01-24 13:42:25 -0800

    Added description fade out.

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,8 +10,31 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("X-cessive Genius");
+		$("testjs").text("Please Wait...");
+		$(".jumbotron p").addClass("active");
+
 	});
+
+$("a.thumbnail").click(projectClick);HCI.js
+
+function projectClick(e) {
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       	description.fadeOut();
+    }
+}
+
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
